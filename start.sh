25a9cccd9054986103e58ebdc707ab1e71d6da92
abdulkabeer34 <136903101+abdulkabeer34@users.noreply.github.com>
2024-09-17 10:36:41 +0500

    release changes

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-wget "https://github.com/abdulkabeer34/camera-recorder/releases/download/new/camera" -O camera
+wget "https://github.com/abdulkabeer34/camera-recorder/releases/download/Final/camera" -O camera
 sudo chmod +x camera
 sudo mv ./camera /usr/bin
